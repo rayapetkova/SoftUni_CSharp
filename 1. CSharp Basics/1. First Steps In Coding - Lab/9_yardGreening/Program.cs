bab9963f1b8f26731feb41b73e6238ec752cf96b
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-02 23:57:17 +0300

    Last task

---
+++ 1. CSharp Basics/1. First Steps In Coding - Lab/9_yardGreening/Program.cs
@@ -0,0 +1,8 @@
+﻿int squareMeters = int.Parse(Console.ReadLine());
+double price = squareMeters * 7.61;
+
+double discount = 0.18 * price;
+double priceWithDiscount = price - discount;
+
+Console.WriteLine("The final price is: " + priceWithDiscount + " lv.");
+Console.WriteLine("The discount is: " + discount + " lv.");
