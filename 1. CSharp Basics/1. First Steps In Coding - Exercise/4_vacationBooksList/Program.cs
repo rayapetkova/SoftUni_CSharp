1a509d0e73fffcf7cab09f5f62e61b95116e6718
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-03 23:45:01 +0300

    Update two new tasks

---
+++ 1. CSharp Basics/1. First Steps In Coding - Exercise/4_vacationBooksList/Program.cs
@@ -0,0 +1,8 @@
+﻿int pagesInBook = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int days = int.Parse(Console.ReadLine());
+
+double hours = pagesInBook / 20;
+double hoursPerDay = hours / days;
+
+Console.WriteLine(hoursPerDay);
---
+++ 1. CSharp Basics/1. First Steps In Coding - Exercise/5_suppliesForSchool/Program.cs
@@ -0,0 +1,12 @@
+﻿int numPens = int.Parse(Console.ReadLine());
+int numMarkers = int.Parse(Console.ReadLine());
+int whiteboartCleaningLiters = int.Parse(Console.ReadLine());
+int discount = int.Parse(Console.ReadLine());
+
+double pensPrice = numPens * 5.80;
+double markersPrice = numMarkers * 7.20;
+double cleaningPrice = whiteboartCleaningLiters * 1.20;
+
+double finalPrice = pensPrice + markersPrice + cleaningPrice;
+double finalPriceWithDiscount = finalPrice - finalPrice * discount / 100;
+Console.WriteLine(finalPriceWithDiscount);
