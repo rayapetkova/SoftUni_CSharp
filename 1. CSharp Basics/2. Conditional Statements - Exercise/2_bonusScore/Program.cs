670e7434707c5869a2f982a7a5e84b481007eb7a
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-05 18:37:57 +0300

    Add first two

---
+++ 1. CSharp Basics/2. Conditional Statements - Exercise/2_bonusScore/Program.cs
@@ -0,0 +1,27 @@
+﻿int points = int.Parse(Console.ReadLine());
+double bonus = 0.0;
+
+if (points <= 100)
+{
+    bonus += 5;
+}
+else if (points > 1000)
+{
+    bonus += 0.10 * points;
+}
+else if (points > 100)
+{
+    bonus += 0.20 * points;
+}
+
+if (points % 2 == 0)
+{
+    bonus += 1;
+}
+else if (points % 10 == 5)
+{
+    bonus += 2;
+}
+
+Console.WriteLine(bonus);
+Console.WriteLine(points + bonus);
