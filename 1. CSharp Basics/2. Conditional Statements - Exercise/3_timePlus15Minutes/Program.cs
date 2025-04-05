75215d91691d22872f2e28eeb2e492ff86ff179f
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-05 18:57:26 +0300

    Third and forth tasks

---
+++ 1. CSharp Basics/2. Conditional Statements - Exercise/3_timePlus15Minutes/Program.cs
@@ -0,0 +1,27 @@
+﻿int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+if (minutes >= 45)
+{
+    hours += 1;
+    minutes = (minutes + 15) % 60;
+}
+else
+{
+    minutes += 15;
+}
+
+
+if (hours == 24) 
+{
+    hours = 0;
+}
+
+if (minutes < 10)
+{
+    Console.WriteLine($"{hours}:0{minutes}");
+}
+else
+{
+    Console.WriteLine($"{hours}:{minutes}");
+}
---
+++ 1. CSharp Basics/2. Conditional Statements - Exercise/4_toyShop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Common;
+
+double tripPrice = double.Parse(Console.ReadLine());
+int puzzels = int.Parse(Console.ReadLine());
+int dolls = int.Parse(Console.ReadLine());
+int bears = int.Parse(Console.ReadLine());
+int minions = int.Parse(Console.ReadLine());
+int trucks = int.Parse(Console.ReadLine());
+
+int order = puzzels + dolls + bears + minions + trucks;
+
+double finalPrice = puzzels * 2.60 + dolls * 3 + bears * 4.10 + minions * 8.20 + trucks * 2;
+
+if (order >= 50)
+{
+    finalPrice -= 0.25 * finalPrice;
+}
+
+finalPrice -= 0.1 * finalPrice;
+
+if (finalPrice >= tripPrice)
+{
+    Console.WriteLine($"Yes! {(finalPrice - tripPrice):F2} lv left.");
+}
+else 
+{
+    Console.WriteLine($"Not enough money! {(tripPrice - finalPrice):F2} lv needed.");
+}
