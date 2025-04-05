6552aae6dcd688c6cf2ff8170f4561365c59d93d
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-05 17:42:01 +0300

    Last task - areas of different figures

---
+++ 1. CSharp Basics/2. Conditional Statements - Lab/7_areaOfFigures/Program.cs
@@ -0,0 +1,32 @@
+﻿string figure = Console.ReadLine();
+
+if (figure == "square")
+{
+    double sideLength = double.Parse(Console.ReadLine());
+
+    double area = sideLength * sideLength;
+    Console.WriteLine(Math.Round(area, 3));
+}
+else if (figure == "rectangle")
+{
+    double firstSide = double.Parse(Console.ReadLine());
+    double secondSide = double.Parse(Console.ReadLine());
+
+    double area = firstSide * secondSide;
+    Console.WriteLine(Math.Round(area, 3));
+}
+else if (figure == "circle")
+{
+    double radius = double.Parse(Console.ReadLine());
+
+    double area = Math.PI * Math.Pow(radius, 2);
+    Console.WriteLine(Math.Round(area, 3));
+}
+else if (figure == "triangle")
+{
+    double sideLength = double.Parse(Console.ReadLine());
+    double height = double.Parse(Console.ReadLine());
+
+    double area = sideLength * height / 2;
+    Console.WriteLine(Math.Round(area, 3));
+}
