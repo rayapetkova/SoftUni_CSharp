100f0d47ff3b97811545fad32ada91a8620c6c39
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-07 13:01:09 +0300

    Add new task

---
+++ 1. CSharp Basics/2. Conditional Statements - Exercise/5_godzillaVsKong/Program.cs
@@ -0,0 +1,24 @@
+﻿double budget = double.Parse(Console.ReadLine());
+int backgroundActors = int.Parse(Console.ReadLine());
+double actorClothingPrice = double.Parse(Console.ReadLine());
+
+double decor = 0.1 * budget;
+double clothingFinalPrice = backgroundActors * actorClothingPrice;
+
+if (backgroundActors > 150)
+{
+    clothingFinalPrice -= 0.1 * clothingFinalPrice;
+}
+
+double finalPrice = clothingFinalPrice + decor;
+
+if (finalPrice > budget)
+{
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {(finalPrice - budget):F2} leva more.");
+}
+else
+{
+    Console.WriteLine("Action!");
+    Console.WriteLine($"Wingard starts filming with {(budget - finalPrice):F2} leva left.");
+}
