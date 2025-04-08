370c82b0977844bd91758daed7207dcd84039238
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-08 22:15:49 +0300

    Add two new tasks from exercise

---
+++ 1. CSharp Basics/3. Conditional Statements Advanced - Exercise/1_cinema/Program.cs
@@ -0,0 +1,24 @@
+﻿string projectionType = Console.ReadLine();
+int rows = int.Parse(Console.ReadLine());
+int cols = int.Parse(Console.ReadLine());
+
+int allSeats = rows * cols;
+
+double ticketPrice = 0.0;
+
+if (projectionType == "Premiere")
+{
+    ticketPrice = 12.00;
+}
+else if (projectionType == "Normal")
+{
+    ticketPrice = 7.50;
+}
+else if (projectionType == "Discount")
+{
+    ticketPrice = 5.00;
+}
+
+double totalPrice = allSeats * ticketPrice;
+
+Console.WriteLine($"{totalPrice:F2} leva");
---
+++ 1. CSharp Basics/3. Conditional Statements Advanced - Exercise/2_summerOutfit/Program.cs
@@ -0,0 +1,52 @@
+﻿int temperature = int.Parse(Console.ReadLine());
+string time = Console.ReadLine();
+
+string outfit = "";
+string shoes = "";
+
+if (temperature >= 10 && temperature <= 18)
+{
+    if (time == "Afternoon" || time == "Evening")
+    {
+        outfit = "Shirt";
+        shoes = "Moccasins";
+    }
+    else if (time == "Morning")
+    {
+        outfit = "Sweatshirt";
+        shoes = "Sneakers";
+    }
+}
+else if (temperature > 18 && temperature <= 24)
+{
+    if (time == "Morning" || time == "Evening")
+    {
+        outfit = "Shirt";
+        shoes = "Moccasins";
+    }
+    else if (time == "Afternoon")
+    {
+        outfit = "T-Shirt";
+        shoes = "Sandals";
+    }
+}
+else if (temperature >= 25)
+{
+    if (time == "Morning")
+    {
+        outfit = "T-Shirt";
+        shoes = "Sandals";
+    }
+    else if (time == "Afternoon")
+    {
+        outfit = "Swim Suit";
+        shoes = "Barefoot";
+    }
+    else if (time == "Evening")
+    {
+        outfit = "Shirt";
+        shoes = "Moccasins";
+    }
+}
+
+Console.WriteLine($"It's {temperature} degrees, get your {outfit} and {shoes}.");
