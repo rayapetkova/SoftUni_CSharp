9fa9b8701e604043650119e64b3e7cf11b950a87
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-08 23:23:43 +0300

    Two more tasks added

---
+++ 1. CSharp Basics/3. Conditional Statements Advanced - Exercise/4_fishingBoat/Program.cs
@@ -0,0 +1,46 @@
+﻿int budget = int.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+int fishmen = int.Parse(Console.ReadLine());
+
+double boatPrice = 0.0;
+
+switch (season)
+{
+    case "Spring":
+        boatPrice = 3000;
+        break;
+    case "Summer":
+    case "Autumn":
+        boatPrice = 4200;
+        break;
+    case "Winter":
+        boatPrice = 2600;
+        break;
+}
+
+if (fishmen <= 6)
+{
+    boatPrice -= 0.1 * boatPrice;
+}
+else if (fishmen <= 11)
+{
+    boatPrice -= 0.15 * boatPrice;
+}
+else
+{
+    boatPrice -= 0.25 * boatPrice;
+}
+
+if (fishmen % 2 == 0 && season != "Autumn")
+{
+    boatPrice -= 0.05 * boatPrice;
+}
+
+if (budget >= boatPrice)
+{
+    Console.WriteLine($"Yes! You have {(budget - boatPrice):F2} leva left.");
+}
+else
+{
+    Console.WriteLine($"Not enough money! You need {(boatPrice - budget):F2} leva.");
+}
---
+++ 1. CSharp Basics/3. Conditional Statements Advanced - Exercise/5_journey/Program.cs
@@ -0,0 +1,48 @@
+﻿double budget = double.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+
+double totalSum = 0.0;
+string destination = "";
+string accommodation = "";
+
+if (budget <= 100)
+{
+    destination = "Bulgaria";
+
+    switch (season)
+    {
+        case "summer":
+            accommodation = "Camp";
+            totalSum += 0.3 * budget;
+            break;
+        case "winter":
+            accommodation = "Hotel";
+            totalSum += 0.7 * budget;
+            break;
+    }
+}
+else if (budget <= 1000)
+{
+    destination = "Balkans";
+
+    switch (season)
+    {
+        case "summer":
+            accommodation = "Camp";
+            totalSum += 0.4 * budget;
+            break;
+        case "winter":
+            accommodation = "Hotel";
+            totalSum += 0.8 * budget;
+            break;
+    }
+}
+else 
+{
+    destination = "Europe";
+    accommodation = "Hotel";
+    totalSum += 0.9 * budget;
+}
+
+Console.WriteLine($"Somewhere in {destination}");
+Console.WriteLine($"{accommodation} - {totalSum:F2}");
