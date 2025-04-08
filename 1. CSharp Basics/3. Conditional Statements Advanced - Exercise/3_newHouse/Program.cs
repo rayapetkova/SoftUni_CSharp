fc6d8307c2423acb69deda9335bd7040f8d5cc79
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-08 22:43:36 +0300

    Third tas

---
+++ 1. CSharp Basics/3. Conditional Statements Advanced - Exercise/3_newHouse/Program.cs
@@ -0,0 +1,61 @@
+﻿string flowers = Console.ReadLine();
+int numberFlowers = int.Parse(Console.ReadLine());
+int budget = int.Parse(Console.ReadLine());
+
+double finalPrice = 0.0;
+
+if (flowers == "Roses")
+{
+    finalPrice = numberFlowers * 5;
+
+    if (numberFlowers > 80)
+    {
+        finalPrice -= 0.1 * finalPrice;
+    }
+}
+else if (flowers == "Dahlias")
+{
+    finalPrice = numberFlowers * 3.80;
+
+    if (numberFlowers > 90)
+    {
+        finalPrice -= 0.15 * finalPrice;
+    }
+}
+else if (flowers == "Tulips")
+{
+    finalPrice = numberFlowers * 2.80;
+
+    if (numberFlowers > 80)
+    {
+        finalPrice -= 0.15 * finalPrice;
+    }
+}
+else if (flowers == "Narcissus")
+{
+    finalPrice = numberFlowers * 3;
+
+    if (numberFlowers < 120)
+    {
+        finalPrice += 0.15 * finalPrice;
+    }
+}
+else if (flowers == "Gladiolus")
+{
+    finalPrice = numberFlowers * 2.50;
+
+    if (numberFlowers < 80)
+    {
+        finalPrice += 0.2 * finalPrice;
+    }
+}
+
+
+if (budget >= finalPrice)
+{
+    Console.WriteLine($"Hey, you have a great garden with {numberFlowers} {flowers} and {(budget - finalPrice):F2} leva left.");
+}
+else
+{
+    Console.WriteLine($"Not enough money, you need {(finalPrice - budget):F2} leva more.");
+}
