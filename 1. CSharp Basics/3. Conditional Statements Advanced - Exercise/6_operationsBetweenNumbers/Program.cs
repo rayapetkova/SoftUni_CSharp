077de7372924f0f2fcb14d12cd7355194ba3ef1e
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-09 00:00:10 +0300

    Add a few more tasks

---
+++ 1. CSharp Basics/3. Conditional Statements Advanced - Exercise/6_operationsBetweenNumbers/Program.cs
@@ -0,0 +1,63 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+string operation = Console.ReadLine();
+
+double result = 0.0;
+
+if (operation == "+")
+{
+    result = num1 + num2;
+
+    if (result % 2 == 0) {
+        Console.WriteLine($"{num1} {operation} {num2} = {result} - even");
+    }
+    else {
+        Console.WriteLine($"{num1} {operation} {num2} = {result} - odd");
+    }
+}
+else if (operation == "-")
+{
+    result = num1 - num2;
+
+    if (result % 2 == 0) {
+        Console.WriteLine($"{num1} {operation} {num2} = {result} - even");
+    }
+    else {
+        Console.WriteLine($"{num1} {operation} {num2} = {result} - odd");
+    }
+}
+else if (operation == "*")
+{
+    result = num1 * num2;
+
+    if (result % 2 == 0) {
+        Console.WriteLine($"{num1} {operation} {num2} = {result} - even");
+    }
+    else {
+        Console.WriteLine($"{num1} {operation} {num2} = {result} - odd");
+    }
+}
+else if (operation == "/")
+{
+    if (num2 == 0)
+    {
+        Console.WriteLine($"Cannot divide {num1} by zero");
+    }
+    else
+    {
+        result = num1 / (double)num2;
+        Console.WriteLine($"{num1} {operation} {num2} = {result:F2}");
+    }
+}
+else if (operation == "%")
+{
+    if (num2 == 0)
+    {
+        Console.WriteLine($"Cannot divide {num1} by zero");
+    }
+    else {
+        result = num1 % (double)num2;
+        Console.WriteLine($"{num1} {operation} {num2} = {result}");
+    }
+    
+}
---
+++ 1. CSharp Basics/3. Conditional Statements Advanced - Exercise/7_hotelRoom/Program.cs
@@ -0,0 +1,45 @@
+﻿string month = Console.ReadLine();
+int nights = int.Parse(Console.ReadLine());
+
+double studioTotalPrice = 0.0;
+double apartmentTotalPrice = 0.0;
+
+if (month == "May" || month == "October")
+{
+    studioTotalPrice = nights * 50;
+    apartmentTotalPrice = nights * 65;
+
+    if (nights > 14)
+    {
+        studioTotalPrice -= 0.3 * studioTotalPrice;
+        apartmentTotalPrice -= 0.1 * apartmentTotalPrice;
+    }
+    else if (nights > 7)
+    {
+        studioTotalPrice -= 0.05 * studioTotalPrice;
+    }
+}
+else if (month == "June" || month == "September")
+{
+    studioTotalPrice = nights * 75.20;
+    apartmentTotalPrice = nights * 68.70;
+
+    if (nights > 14)
+    {
+        studioTotalPrice -= 0.2 * studioTotalPrice;
+        apartmentTotalPrice -= 0.1 * apartmentTotalPrice;
+    }
+}
+else if (month == "July" || month == "August")
+{
+    studioTotalPrice = nights * 76;
+    apartmentTotalPrice = nights * 77;
+
+    if (nights > 14)
+    {
+        apartmentTotalPrice -= 0.1 * apartmentTotalPrice;
+    }
+}
+
+Console.WriteLine($"Apartment: {apartmentTotalPrice:F2} lv.");
+Console.WriteLine($"Studio: {studioTotalPrice:F2} lv.");
