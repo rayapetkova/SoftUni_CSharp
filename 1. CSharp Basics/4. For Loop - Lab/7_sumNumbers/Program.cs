6745b7832e9f223198487b6bd664a418d6687be4
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-13 23:31:14 +0300

    Add 2 more tasks

---
+++ 1. CSharp Basics/4. For Loop - Lab/7_sumNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿int numbersCount = int.Parse(Console.ReadLine());
+
+int totalSum = 0;
+
+for (int n = 0; n < numbersCount; n++)
+{
+    int number = int.Parse(Console.ReadLine());
+    totalSum += number;
+}
+
+Console.WriteLine(totalSum);
---
+++ 1. CSharp Basics/4. For Loop - Lab/8_numberSequence/Program.cs
@@ -0,0 +1,22 @@
+﻿int numbersCount = int.Parse(Console.ReadLine());
+
+int maxNum = int.MinValue;
+int minNum = int.MaxValue;
+
+for (int n = 0; n < numbersCount; n++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (number > maxNum)
+    {
+        maxNum = number;
+    }
+
+    if (number < minNum)
+    {
+        minNum = number;
+    }
+}
+
+Console.WriteLine($"Max number: {maxNum}");
+Console.WriteLine($"Min number: {minNum}");
