a8e306c5ae37841ceed7a7246d956fd77602c78d
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-13 23:09:45 +0300

    Add 2 more tasks

---
+++ 1. CSharp Basics/4. For Loop - Lab/6_vowelsSum/Program.cs
@@ -0,0 +1,24 @@
+﻿string text = Console.ReadLine();
+
+Dictionary<string, int> vowels = new Dictionary<string, int>
+{
+    {"a", 1},
+    {"e", 2},
+    {"i", 3},
+    {"o", 4},
+    {"u", 5}
+};
+
+int totalSum = 0;
+
+for (int i = 0; i < text.Length; i++)
+{
+    string character = text[i].ToString();
+
+    if (vowels.ContainsKey(character))
+    {
+        totalSum += vowels[character];
+    }
+}
+
+Console.WriteLine(totalSum);
