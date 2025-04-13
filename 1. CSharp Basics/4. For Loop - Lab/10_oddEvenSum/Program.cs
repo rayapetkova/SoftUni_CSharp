552823e798995c102d4151faaf33b9d42dd2be5f
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-13 23:46:04 +0300

    Add last two tasks - 9 and 10

---
+++ 1. CSharp Basics/4. For Loop - Lab/10_oddEvenSum/Program.cs
@@ -0,0 +1,29 @@
+﻿int numbersCount = int.Parse(Console.ReadLine());
+
+int evenSum = 0;
+int oddSum = 0;
+
+for (int i = 0; i < numbersCount; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (i % 2 == 0)
+    {
+        evenSum += number;
+    }
+    else
+    {
+        oddSum += number;
+    }
+}
+
+if (evenSum == oddSum)
+{
+    Console.WriteLine("Yes");
+    Console.WriteLine($"Sum = {evenSum}");
+}
+else 
+{
+    Console.WriteLine("No");
+    Console.WriteLine($"Diff = {Math.Abs(evenSum - oddSum)}");
+}
---
+++ 1. CSharp Basics/4. For Loop - Lab/9_leftAndRightSum/Program.cs
@@ -0,0 +1,26 @@
+﻿int nums = int.Parse(Console.ReadLine());
+
+int leftSum = 0;
+int rightSum = 0;
+
+for (int i = 0; i < nums; i++)
+{
+    int num1 = int.Parse(Console.ReadLine());
+    leftSum += num1;
+}
+
+for (int j = 0; j < nums; j++)
+{
+    int num2 = int.Parse(Console.ReadLine());
+    rightSum += num2;
+}
+
+if (leftSum == rightSum)
+{
+    Console.WriteLine($"Yes, sum = {leftSum}");
+}
+else
+{
+    int difference = Math.Abs(leftSum - rightSum);
+    Console.WriteLine($"No, diff = {difference}");
+}
