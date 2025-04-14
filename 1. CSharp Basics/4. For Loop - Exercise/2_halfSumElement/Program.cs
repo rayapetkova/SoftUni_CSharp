53597e81f526c5a1b5ee80fd6d26a4a98ac899c1
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-14 20:48:44 +0300

    Add 2 new tasks

--- 1. CSharp Basics/4. For Loop - Exercise/2_halfSumElement/Program.cs
+++ 1. CSharp Basics/4. For Loop - Exercise/2_halfSumElement/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿int numbersCount = int.Parse(Console.ReadLine());
+
+int biggestNum = int.MinValue;
+int sum = 0;
+
+for (int i = 0; i < numbersCount; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (number > biggestNum)
+    {
+        biggestNum = number;
+    }
+
+    sum += number;
+}
+
+sum -= biggestNum;
+
+if (biggestNum == sum)
+{
+    Console.WriteLine("Yes");
+    Console.WriteLine($"Sum = {sum}");
+}
+else
+{
+    Console.WriteLine("No");
+    Console.WriteLine($"Diff = {Math.Abs(biggestNum - sum)}");
+}
---
+++ 1. CSharp Basics/4. For Loop - Exercise/3_histogram/Program.cs
@@ -0,0 +1,45 @@
+﻿int numbersCount = int.Parse(Console.ReadLine());
+
+double under200 = 0.0;
+double under399 = 0.0;
+double under599 = 0.0;
+double under799 = 0.0;
+double above800 = 0.0;
+
+for (int i = 0; i < numbersCount; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (number < 200)
+    {
+        under200 += 1;
+    }
+    else if (number <= 399)
+    {
+        under399 += 1;
+    }
+    else if (number <= 599)
+    {
+        under599 += 1;
+    }
+    else if (number <= 799)
+    {
+        under799 += 1;
+    }
+    else 
+    {
+        above800 += 1;
+    }
+}
+
+double under200Percents = under200 / numbersCount * 100;
+double under399Percents = under399 / numbersCount * 100;
+double under599Percents = under599 / numbersCount * 100;
+double under799Percents = under799 / numbersCount * 100;
+double above800Percents = above800 / numbersCount * 100;
+
+Console.WriteLine($"{under200Percents:F2}%");
+Console.WriteLine($"{under399Percents:F2}%");
+Console.WriteLine($"{under599Percents:F2}%");
+Console.WriteLine($"{under799Percents:F2}%");
+Console.WriteLine($"{above800Percents:F2}%");
