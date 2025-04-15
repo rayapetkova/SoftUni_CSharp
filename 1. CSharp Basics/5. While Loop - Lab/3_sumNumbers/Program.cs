20e0a185c216ed4423bea163e68370fd5a11811a
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-15 22:54:42 +0300

    Add two new tasks - 3 and 4

---
+++ 1. CSharp Basics/5. While Loop - Lab/3_sumNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿int firstNum = int.Parse(Console.ReadLine());
+
+int number = int.Parse(Console.ReadLine());
+int sum = number;
+
+while (sum < firstNum)
+{
+    number = int.Parse(Console.ReadLine());
+    sum += number;
+}
+
+Console.WriteLine(sum);
