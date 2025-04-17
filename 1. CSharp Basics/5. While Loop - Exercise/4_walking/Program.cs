c83041e14d69eb97d7fe7ccd35f0b46149a66ab4
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-17 18:26:50 +0300

    Add one more task - task 4

---
+++ 1. CSharp Basics/5. While Loop - Exercise/4_walking/Program.cs
@@ -0,0 +1,35 @@
+﻿int goal = 10000;
+int dailySteps = 0;
+
+bool goingHome = false;
+
+while (true)
+{
+    string input = Console.ReadLine();
+    int steps = 0;
+
+    if (input == "Going home")
+    {
+        steps = int.Parse(Console.ReadLine());
+        goingHome = true;
+    }
+    else
+    {
+        steps = int.Parse(input);
+    }
+    
+    dailySteps += steps;
+
+    if (dailySteps >= goal)
+    {
+        Console.WriteLine("Goal reached! Good job!");
+        Console.WriteLine($"{dailySteps - goal} steps over the goal!");
+        break;
+    }
+
+    if (goingHome)
+    {
+        Console.WriteLine($"{goal - dailySteps} more steps to reach goal.");
+        break;
+    }
+}
