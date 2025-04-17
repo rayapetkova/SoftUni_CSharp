0dc2333360fba2cc7a968876dd409bd049f824ca
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-18 00:17:20 +0300

    Add to new tasks

---
+++ 1. CSharp Basics/5. While Loop - Exercise/6_cake/Program.cs
@@ -0,0 +1,26 @@
+﻿int width = int.Parse(Console.ReadLine());
+int length = int.Parse(Console.ReadLine());
+
+int allPieces = width * length;
+
+bool stopped = false;
+
+while (allPieces > 0)
+{
+    string input = Console.ReadLine();
+
+    if (input == "STOP")
+    {
+        Console.WriteLine($"{allPieces} pieces are left.");
+        stopped = true;
+        break;
+    }
+
+    int pieces = int.Parse(input);
+    allPieces -= pieces;
+}
+
+if (!stopped)
+{
+    Console.WriteLine($"No more cake left! You need {Math.Abs(allPieces)} pieces more.");
+}
