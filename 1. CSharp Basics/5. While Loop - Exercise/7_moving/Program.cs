5ef2fe11d967f8a8609091e92e66f44fcc60e989
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-18 00:33:47 +0300

    Task 7

---
+++ 1. CSharp Basics/5. While Loop - Exercise/7_moving/Program.cs
@@ -0,0 +1,27 @@
+﻿int width = int.Parse(Console.ReadLine());
+int length = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+
+int space = width * length * height;
+
+int cartonsSpace = 0;
+
+while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input == "Done")
+    {
+        Console.WriteLine($"{space - cartonsSpace} Cubic meters left.");
+        break;
+    }
+
+    int cartonsCount = int.Parse(input);
+    cartonsSpace += cartonsCount;
+
+    if (cartonsSpace >= space)
+    {
+        Console.WriteLine($"No more free space! You need {cartonsSpace - space} Cubic meters more.");
+        break;
+    }
+}
