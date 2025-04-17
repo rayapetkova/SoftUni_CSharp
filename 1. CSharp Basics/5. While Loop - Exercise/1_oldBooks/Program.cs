b1e582a1366b3af2e1a24d95656c5ea14fe1aae5
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-17 18:11:01 +0300

    Add two new tasks

--- 1. CSharp Basics/5. While Loop - Exercise/1_oldBooks/Program.cs
+++ 1. CSharp Basics/5. While Loop - Exercise/1_oldBooks/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string favouriteBook = Console.ReadLine();
+
+int allBooks = 0;
+
+string input = Console.ReadLine();
+
+while (input != favouriteBook)
+{
+
+    if (input == "No More Books")
+    {
+        Console.WriteLine("The book you search is not here!");
+        Console.WriteLine($"You checked {allBooks} books.");
+        break;
+    }
+
+    input = Console.ReadLine();
+
+    allBooks += 1;
+}
+
+if (input == favouriteBook)
+{
+    Console.WriteLine($"You checked {allBooks} books and found it.");
+}
---
+++ 1. CSharp Basics/5. While Loop - Exercise/2_examPreparation/Program.cs
@@ -0,0 +1,38 @@
+﻿int badGradesCount = int.Parse(Console.ReadLine());
+
+int gradesSum = 0;
+int gradesCount = 0;
+int badGradesTotal = 0;
+string lastProblem = "";
+
+while (true)
+{
+    string task = Console.ReadLine();
+
+    if (task == "Enough")
+    {
+        Console.WriteLine($"Average score: {((double)gradesSum / gradesCount):F2}");
+        Console.WriteLine($"Number of problems: {gradesCount}");
+        Console.WriteLine($"Last problem: {lastProblem}");
+        break;
+    }
+
+    lastProblem = task;
+
+    int grade = int.Parse(Console.ReadLine());
+
+    if (grade <= 4)
+    {
+        badGradesTotal += 1;
+    }
+
+    if (badGradesTotal == badGradesCount)
+    {
+        Console.WriteLine($"You need a break, {badGradesTotal} poor grades.");
+        break;
+    }
+
+    gradesSum += grade;
+    gradesCount += 1;
+
+}
