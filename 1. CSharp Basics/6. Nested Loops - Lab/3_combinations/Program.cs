d029fbeebeeb616bd5b6d3f8266f8d21317f1b2a
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-19 01:01:10 +0300

    Add third task - combinations

---
+++ 1. CSharp Basics/6. Nested Loops - Lab/3_combinations/Program.cs
@@ -0,0 +1,19 @@
+﻿int expectedResult = int.Parse(Console.ReadLine());
+
+int combinations = 0;
+
+for (int x1 = 0; x1 <= expectedResult; x1++)
+{
+    for (int x2 = 0; x2 <= expectedResult; x2++)
+    {
+        for (int x3 = 0; x3 <= expectedResult; x3++)
+        {
+            if (x1 + x2 + x3 == expectedResult)
+            {
+                combinations++;
+            }
+        }
+    }
+}
+
+Console.WriteLine(combinations);
