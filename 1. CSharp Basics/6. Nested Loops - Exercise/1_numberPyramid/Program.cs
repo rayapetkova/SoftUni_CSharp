5927e0e5d028693618f68e3cd875a9aec955bc59
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-19 23:03:05 +0300

    Add first task (interesting one)

---
+++ 1. CSharp Basics/6. Nested Loops - Exercise/1_numberPyramid/Program.cs
@@ -0,0 +1,30 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int numsOnRow = 1;
+int currentNum = 1;
+bool isBigger = false;
+
+while (true)
+{
+    string row = "";
+
+    for (int n = currentNum; n < currentNum + numsOnRow; n++)
+    {
+        if (n > number)
+        {
+            isBigger = true;
+            break;
+        }
+        row += $"{n} ";
+    }
+
+    currentNum += numsOnRow;
+    numsOnRow++;
+
+    Console.WriteLine(row);
+
+    if (isBigger)
+    {
+        break;
+    }
+}
