6d2513361e2fd36a5f860132b792e49cce8afc55
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-22 21:04:49 +0300

    Update tasks and add second one

--- 1. CSharp Basics/3. Conditional Statements Advanced - Lab/5_smallShop/obj/Debug/net9.0/5_smallShop.AssemblyInfo.cs
+++ 1. CSharp Basics/3. Conditional Statements Advanced - Lab/5_smallShop/obj/Debug/net9.0/5_smallShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("5_smallShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57d19b352d5e33a9e5367f7ad313fe27aa12eb16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5927e0e5d028693618f68e3cd875a9aec955bc59")]
 [assembly: System.Reflection.AssemblyProductAttribute("5_smallShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("5_smallShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 1. CSharp Basics/6. Nested Loops - Exercise/2_equalSumsEvenOddPosition/Program.cs
@@ -0,0 +1,33 @@
+﻿int firstNum = int.Parse(Console.ReadLine());
+int secondNum = int.Parse(Console.ReadLine());
+
+string finalAnswer = "";
+
+for (int num = firstNum; num <= secondNum; num++)
+{
+    string number = num.ToString();
+
+    int evenSum = 0;
+    int oddSum = 0;
+
+    for (int i = 0; i < number.Length; i++)
+    {
+        int n = int.Parse(number[i].ToString());
+
+        if (i % 2 == 0)
+        {
+            oddSum += n;
+        }
+        else
+        {
+            evenSum += n;
+        }
+    }
+
+    if (evenSum == oddSum)
+    {
+        finalAnswer += $"{num} ";
+    }
+}
+
+Console.WriteLine(finalAnswer);
