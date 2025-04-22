80f9ee6d0fb7b47073c4fc1f425ce01d0e5b4ee0
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-22 22:38:44 +0300

    Add two new tasks

---
+++ 1. CSharp Basics/6. Nested Loops - Exercise/5_specialNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+string finalAnswer = "";
+
+for (int num = 1111; num <= 9999; num++)
+{
+    string numStr = num.ToString();
+    bool isSpecial = true;
+
+    for (int i = 0; i < numStr.Length; i++)
+    {
+        int digit = int.Parse(numStr[i].ToString());
+
+        if (digit == 0 || number % digit != 0)
+        {
+            isSpecial = false;
+            break;
+        }
+    }
+
+    if (isSpecial)
+    {
+        finalAnswer += $"{num} ";
+    }
+}
+
+Console.WriteLine(finalAnswer);
---
+++ 1. CSharp Basics/6. Nested Loops - Exercise/6_cinemaTickets/Program.cs
@@ -0,0 +1,62 @@
+﻿int allTickets = 0;
+int allStudent = 0;
+int allStandard = 0;
+int allKid = 0;
+
+while (true)
+{
+    string movie = Console.ReadLine();
+
+    if (movie == "Finish")
+    {
+        break;
+    }
+
+    int seats = int.Parse(Console.ReadLine());
+    int totalTickets = 0;
+
+    while (true)
+    {
+        string typeTicket = Console.ReadLine();
+
+        if (typeTicket == "End")
+        {
+            break;
+        }
+        else if (typeTicket == "student")
+        {
+            allStudent++;
+            totalTickets++;
+        }
+        else if (typeTicket == "standard")
+        {
+            allStandard++;
+            totalTickets++;
+        }
+        else if (typeTicket == "kid")
+        {
+            allKid++;
+            totalTickets++;
+        }
+
+        if (totalTickets >= seats)
+        {
+            break;
+        }
+    }
+
+    allTickets += totalTickets;
+
+    double percentsFull = ((double)totalTickets * 100) / seats;
+    Console.WriteLine($"{movie} - {percentsFull:F2}% full.");
+    
+}
+
+double studentPercents = ((double)allStudent * 100) / allTickets;
+double standardPercents = ((double)allStandard * 100) / allTickets;
+double kidPercents = ((double)allKid * 100) / allTickets;
+
+Console.WriteLine($"Total tickets: {allTickets}");
+Console.WriteLine($"{studentPercents:F2}% student tickets.");
+Console.WriteLine($"{standardPercents:F2}% standard tickets.");
+Console.WriteLine($"{kidPercents:F2}% kids tickets.");
