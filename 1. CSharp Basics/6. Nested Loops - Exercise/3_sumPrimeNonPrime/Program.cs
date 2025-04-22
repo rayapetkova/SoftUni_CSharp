3732f0a980845d73155d29113b70ef920df0a6ed
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-22 21:47:42 +0300

    Add third and forth tasks

---
+++ 1. CSharp Basics/6. Nested Loops - Exercise/3_sumPrimeNonPrime/Program.cs
@@ -0,0 +1,42 @@
+﻿int primeSum = 0;
+int nonPrimeSum = 0;
+
+while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input == "stop")
+    {
+        break;
+    }
+
+    int number = int.Parse(input);
+    bool prime = true;
+
+    if (number < 0)
+    {
+        Console.WriteLine("Number is negative.");
+        continue;
+    }
+
+    for (int n = 2; n <= Math.Sqrt(number); n++)
+    {
+        if (number % n == 0)
+        {
+            prime = false;
+            break;
+        }
+    }
+
+    if (prime)
+    {
+        primeSum += number;
+    }
+    else
+    {
+        nonPrimeSum += number;
+    }
+}
+
+Console.WriteLine($"Sum of all prime numbers is: {primeSum}");
+Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
---
+++ 1. CSharp Basics/6. Nested Loops - Exercise/4_trainTheTrainers/Program.cs
@@ -0,0 +1,29 @@
+﻿int jury = int.Parse(Console.ReadLine());
+
+int allPresentationsRatesCount = 0;
+double totalRates = 0;
+
+while (true)
+{
+    string presentation = Console.ReadLine();
+
+    if (presentation == "Finish")
+    {
+        break;
+    }
+
+    double allRates = 0;
+
+    for (int i = 0; i < jury; i++)
+    {
+        double rate = double.Parse(Console.ReadLine());
+
+        allRates += rate;
+        totalRates += rate;
+        allPresentationsRatesCount++;
+    }
+
+    Console.WriteLine($"{presentation} - {(allRates / jury):F2}");
+}
+
+Console.WriteLine($"Student's final assessment is {(totalRates / allPresentationsRatesCount):F2}");
