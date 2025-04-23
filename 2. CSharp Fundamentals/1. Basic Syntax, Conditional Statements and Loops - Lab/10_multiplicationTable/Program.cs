b007bb1111d3e5f72c189731f6ea1404f9ad4510
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-04-23 23:46:31 +0300

    Two more tasks

---
+++ 2. CSharp Fundamentals/1. Basic Syntax, Conditional Statements and Loops - Lab/10_multiplicationTable/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+for (int secondNum = 1; secondNum <= 10; secondNum++)
+{
+    Console.WriteLine($"{number} X {secondNum} = {number * secondNum}");
+}
---
+++ 2. CSharp Fundamentals/1. Basic Syntax, Conditional Statements and Loops - Lab/9_sumOfOddNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int number = 1;
+int allNums = 0;
+
+int oddNumbersSum = 0;
+
+while (allNums < n)
+{
+    oddNumbersSum += number;
+
+    Console.WriteLine(number);
+
+    number += 2;
+    allNums++;
+}
+
+Console.WriteLine($"Sum: {oddNumbersSum}");
