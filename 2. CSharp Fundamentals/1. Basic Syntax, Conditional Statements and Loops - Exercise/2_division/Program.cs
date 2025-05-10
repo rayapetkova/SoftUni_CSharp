92843cea3d1ff5e7e4e05cf13bbcdf7de12ba6bc
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-05-10 22:56:37 +0300

    Task new

---
+++ 2. CSharp Fundamentals/1. Basic Syntax, Conditional Statements and Loops - Exercise/2_division/Program.cs
@@ -0,0 +1,37 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int divisibleBy = 0;
+
+if (number % 2 == 0)
+{
+    divisibleBy = 2;
+}
+
+if (number % 3 == 0)
+{
+    divisibleBy = 3;
+}
+
+if (number % 6 == 0)
+{
+    divisibleBy = 6;
+}
+
+if (number % 7 == 0)
+{
+    divisibleBy = 7;
+}
+
+if (number % 10 == 0)
+{
+    divisibleBy = 10;
+}
+
+if (divisibleBy != 0)
+{
+    Console.WriteLine($"The number is divisible by {divisibleBy}");
+}
+else 
+{
+    Console.WriteLine("Not divisible.").
+}
