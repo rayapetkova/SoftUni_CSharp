e7d0200f87e048926cd894ef47a2c8754725b7b1
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2025-05-10 22:46:06 +0300

    Add task

--- 2. CSharp Fundamentals/1. Basic Syntax, Conditional Statements and Loops - Exercise/1_ages/Program.cs
+++ 2. CSharp Fundamentals/1. Basic Syntax, Conditional Statements and Loops - Exercise/1_ages/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int age = int.Parse(Console.ReadLine());
+
+if (age >= 0 && age <= 2)
+{
+    Console.WriteLine("baby");
+}
+else if (age <= 13)
+{
+    Console.WriteLine("child");
+}
+else if (age <= 19)
+{
+    Console.WriteLine("teenager");
+}
+else if (age <= 65)
+{
+    Console.WriteLine("adult");
+}
+else 
+{
+    Console.WriteLine("elder");
+}
